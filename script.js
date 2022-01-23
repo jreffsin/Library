b474b124df9ac89207a8278944a45d82608dc6b6
Jesse Reffsin <jreffsin@gmail.com>
2022-01-23 18:06:44 -0500

    implement create table logic

---
+++ script.js
@@ -0,0 +1,84 @@
+//next things todo:
+//make it so that ids of rows and cells change when rows are deleted
+//(otherwise deleting a row will cause display table and myLibrary array to be out of sync, 
+//which will be a problem once user has ability to add elements themselves)
+
+//also need to create input form for users to add new elements to table
+
+
+let myLibrary = [];
+let tableRowElements = 6;
+
+function Book(title, author, pages, read, id) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+    this.id = id;
+}
+
+function addBookToLibrary (title, author, pages, read) {
+    let newId = myLibrary.length;
+    let newBook = new Book(title, author, pages, read, newId);
+    myLibrary.push(newBook);
+}
+
+function populateTable(){
+    for (let i = 0; i < myLibrary.length; i++){
+
+        //get table and add row element to it
+        let table = document.getElementById('libaryTable');
+        let row = table.insertRow(-1);
+        row.setAttribute('id', `row${i}`);
+    
+        //fill row with book data
+        let cell1 = row.insertCell(0);
+        cell1.textContent = myLibrary[i].title;
+        let cell2 = row.insertCell(1);
+        cell2.textContent = myLibrary[i].author;
+        let cell3 = row.insertCell(2);
+        cell3.textContent = myLibrary[i].pages;
+        let cell4 = row.insertCell(3);
+        cell4.setAttribute('id', `read_status${i}`);
+        if (myLibrary[i].read) {
+            cell4.textContent = 'yes';
+        } else {
+            cell4.textContent = 'no';
+        }
+    
+        //add read status toggle button
+        let newReadToggleBtn = document.createElement('button');
+        newReadToggleBtn.textContent='Toggle Read Status';
+        newReadToggleBtn.setAttribute('class', 'read_toggle_btn');
+        newReadToggleBtn.setAttribute('id', i);
+        newReadToggleBtn.addEventListener('click', function(e) {
+            let readStatus = document.getElementById(`read_status${this.id}`);
+            if (readStatus.textContent == 'yes'){
+                readStatus.textContent = 'no';
+                return;
+            }
+            readStatus.textContent = 'yes';
+        })
+        let cell5 = row.insertCell(4);
+        cell5.appendChild(newReadToggleBtn);
+    
+        //add row delete button
+        let newDeleteRowBtn = document.createElement('button');
+        newDeleteRowBtn.textContent='Delete Entry';
+        newDeleteRowBtn.setAttribute('class', 'del_row_btn');
+        newDeleteRowBtn.setAttribute('id', i);
+        newDeleteRowBtn.addEventListener('click', function(e) {
+            let targetRow = document.getElementById(`row${this.id}`);
+            targetRow.remove();
+            myLibrary.splice(i, 1);
+        })
+        let cell6 = row.insertCell(5);
+        cell6.appendChild(newDeleteRowBtn);
+    }
+}
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkein', 310, true);
+addBookToLibrary("Ender's Game", 'Orson Scott Card', 324, true);
+addBookToLibrary('Moby Dick', 'Herman Melville', 427, false);
+
+populateTable();
